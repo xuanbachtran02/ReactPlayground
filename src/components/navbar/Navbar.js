9d15c2765f98d13bdcb6ccd2296b43c0940c7766
De Anza CPC <83796054+xuanbachtran02@users.noreply.github.com>
2021-12-30 21:30:20 -0800

    fix css element

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,7 +1,7 @@
 import React, {Component} from 'react';
 import './Navbar.css';
-import {MenuItems} from './MenuItems'
-import {DropdownItems} from './DropdownItems'
+import { MenuItems } from './MenuItems'
+import { DropdownItems } from './DropdownItems'
 import { Container } from 'reactstrap';
 import { Navbar, Nav, NavDropdown } from 'react-bootstrap'
 import { Link } from 'react-router-dom';
